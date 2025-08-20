302524bdfefa88b86ae9c8e28c7767863c9e6838
xunfen <107894831+xunfen@users.noreply.github.com>
2025-08-20 17:46:04 +0800

    Add files via upload

--- 成风的Java学习/力扣/T1.java
+++ 成风的Java学习/力扣/T1.java
@@ -15,6 +15,15 @@ public class T1 {
 输入：nums = [3,3], target = 6
 输出：[0,1]
          */
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    System.out.print(i+","+j);
+                }
+            }
+        }
 
          
     }
