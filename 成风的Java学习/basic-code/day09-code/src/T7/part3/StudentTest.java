a4813cd05d8e83bb82b7c826fb1aa85f48f8a53d
xunfen <107894831+xunfen@users.noreply.github.com>
2025-08-23 21:56:23 +0800

    Add files via upload

--- 成风的Java学习/basic-code/day09-code/src/T7/part3/StudentTest.java
+++ 成风的Java学习/basic-code/day09-code/src/T7/part3/StudentTest.java
@@ -13,8 +13,43 @@ public class StudentTest {
         arr[2] = stu3;
 
         //要求5：查询id为2的学生，如果存在，则将他的年龄+1
-        
+        int index = getIndex(arr,2);
+        if (index >= 0) {
+            Student stu = arr[index];
+            int newAge = stu.getAge() + 1;
+            stu.setAge(newAge);
+            //遍历数组
+            printArr(arr);
+        } else {
+            System.out.println("没有找到此id学生");
+        }
 
 
+
+    }
+
+    //找到id在数组中的索引
+    public static int getIndex(Student[] arr , int id){
+        for (int i = 0; i < arr.length; i++) {
+            Student stu = arr[i];
+            if (stu !=null) {
+                int sid = stu.getId();
+                if (sid == id) {
+                    return i;
+                }
+            }
+        }
+        //循环结束，没有找到,表示不存在
+        return -1;
+
+    }
+
+    public static void printArr(Student[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            Student stu = arr[i];
+            if (stu != null) {
+                System.out.println(arr[i].getId() + ", " + arr[i].getName() + ", " + arr[i].getAge());
+            }
+        }
     }
 }
