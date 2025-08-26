adec0bd5f2b607361cb5c8cec677519b9b300cf2
xunfen <107894831+xunfen@users.noreply.github.com>
2025-08-26 23:54:00 +0800

    Add files via upload

--- 成风的Java学习/basic-code/day10-code/PART2/StringBuilderDemo2.java
+++ 成风的Java学习/basic-code/day10-code/PART2/StringBuilderDemo2.java
@@ -1,3 +1,14 @@
 public class StringBuilderDemo2 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("aaa");
+        sb.append("bbb");
+        sb.append("ccc");
+        sb.append("ddd");
+        //把StringBUilder变回字符串
+        String str = sb.toString();
+        System.out.println(str);
+
+    }
     
 }
